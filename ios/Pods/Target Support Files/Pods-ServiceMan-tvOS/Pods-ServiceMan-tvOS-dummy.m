aa914115513a0c7e11a477b9155fe605288723cf
MobileDev418 <oncemoreforyou@outlook.com>
2020-01-23 04:25:34 +0800

    initial project

---
+++ ios/Pods/Target Support Files/Pods-ServiceMan-tvOS/Pods-ServiceMan-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ServiceMan_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_ServiceMan_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-ServiceMan/Pods-ServiceMan-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ServiceMan : NSObject
+@end
+@implementation PodsDummy_Pods_ServiceMan
+@end
