d3df25bb97defd357f7d21910982c097856c63a0
MobileDev418 <oncemoreforyou@outlook.com>
2020-01-25 00:38:08 +0800

    global variables and components

---
+++ src/source/global.js
@@ -0,0 +1,36 @@
+
+export class App {
+    static isOpenMenu = false
+    static isCustomer = 2
+    static supportID = 100
+    
+    static google_place_api = "https://maps.google.com/maps/api/geocode/json?key=AIzaSyDQnNu7t79jqzhO7rt7_SyHwWSXfcbj35k"
+    
+    static terms_link = "http://www.servicemanapp.com/terms.pdf"
+    static topup_link = "http://www.servicemanapp.com/topup.pdf"
+    
+    static api_link = "http://firebase.stage02.obdemo.com:3007/"
+    
+    static landing_page = ""
+    static sub_langing_page = ""
+    
+    // Bid
+    static post_id = ""
+    static amount = 0
+    static fee = "Labour fee"
+    static notes = ""
+
+    // Post
+    static images = ""
+    static price = ""
+    static notes = ""
+    static property_id = -1
+    static address = ""
+    static code = ""
+
+    // Job settings
+    static isResaved = false
+
+    //Facebook login
+    static isFacebook = false
+}
