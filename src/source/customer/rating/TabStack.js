ead528dc1b554acc38dc7f8ed5f078fe0cfad0c0
MobileDev418 <oncemoreforyou@outlook.com>
2020-01-31 10:28:49 +0800

    rating

---
+++ src/source/customer/rating/TabStack.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation'
+
+import Rating from "../../Rating";
+
+const Navigation = createStackNavigator({
+    
+    Rating: { screen: Rating
+    }
+
+}, {
+    initialRouteName: 'Rating',
+    /* The header config from FirstScreen is now here */
+    navigationOptions: {
+
+        headerStyle: {
+            backgroundColor: '#fbf1dc',
+        },
+        headerTintColor: '#000000',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        },
+    },
+});
+
+const TabStack = createAppContainer(Navigation);
+
+export default TabStack;
+
